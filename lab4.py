b608a54cbc1c51127461420d10a1441773b79ede
mxd17 <128420738+Madhu172005@users.noreply.github.com>
2025-10-28 10:18:15 +0530

    Create lab4.py

---
+++ lab4.py
@@ -0,0 +1,14 @@
+import nltk
+from nltk import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('punkt_tab')
+
+
+text = "The cats are chasing mice."
+tokens = word_tokenize(text)
+lemmatizer = WordNetLemmatizer()
+lemmas = [lemmatizer.lemmatize(token) for token in tokens]
+print("Lemmas:", lemmas)
